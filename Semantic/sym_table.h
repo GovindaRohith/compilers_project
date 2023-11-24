d27348dc2863caee48b671cc74da7922fe4fd56a
GovindaRohith <102508383+GovindaRohith@users.noreply.github.com>
2023-11-24 18:05:36 +0530

    Update sym_table.h

--- Semantic/sym_table.h
+++ Semantic/sym_table.h
@@ -1,1 +1,20 @@
+#ifndef SYM_TABLE_H
+#define SYM_TABLE_H
 
+#include<bits/stdc++.h>
+using namespace std;
+// #include "sym.h"
+// loc_Sym_tab
+class args {
+    public:
+        string name;
+        pair<short int ,bool> dat_type;
+        // short int int/float/double/cdouble 
+        //bool is_arr
+} ;
+class Fn_Symbol{
+    public: 
+    string fn_name;
+    pair<short int,bool> ret_type;
+    list <args> arg_list;
+};
